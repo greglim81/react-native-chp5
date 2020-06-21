07f18f96042205abb4ffdbc387e06d1d46710797
Greg Lim <limjunqu@gmail.com>
2020-06-21 15:32:07 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Header, Content, Body, Title } from 'native-base';
+import ProductList from './ProductList';
+
+export default function App() {
+
+  const isValid = true;
+
+  return (
+    <Container>
+      <Header>
+        <Body>
+          <Title>List of Products</Title>
+        </Body>      
+      </Header>      
+      <Content>
+        <ProductList />
+      </Content>
+    </Container>
+  );
+}
